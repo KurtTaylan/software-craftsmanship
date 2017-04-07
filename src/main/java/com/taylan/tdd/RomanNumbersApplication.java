3e31aa9e09ed28f91d620e5c2b41c585ed5145da
Taylan Kurt <taylankurt34@gmail.com>
2017-04-07 16:47:03 +0300

    1/9: 3 successful tests are passed!

--- src/main/java/com/taylan/tdd/RomanNumbersApplication.java
+++ src/main/java/com/taylan/tdd/RomanNumbersApplication.java
@@ -9,7 +9,10 @@ public class RomanNumbersApplication {
 
 
     public String convertToRomanFrom(int number) {
-        if(number > 1)
+
+        if(number == 3)
+            return "III";
+        if(number == 2)
             return  "II";
         else
             return "I";
--- src/test/java/com/taylan/tdd/RomanNumbersApplicationTest.java
+++ src/test/java/com/taylan/tdd/RomanNumbersApplicationTest.java
@@ -4,14 +4,17 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Main Test
+ */
 public class RomanNumbersApplicationTest {
 
 
     @Test
     public void should_number_one_converted_roman_numeric() {
         int numberOne = 1;
-        RomanNumbersApplication converter = new RomanNumbersApplication();
 
+        RomanNumbersApplication converter = new RomanNumbersApplication();
         String romanNumberResult = converter.convertToRomanFrom(numberOne);
 
         assertThat(romanNumberResult).isSameAs("I");
@@ -21,10 +24,20 @@ public class RomanNumbersApplicationTest {
     @Test
     public void should_number_two_converted_roman_numeric() throws Exception {
         int numberTwo = 2;
-        RomanNumbersApplication converter = new RomanNumbersApplication();
 
+        RomanNumbersApplication converter = new RomanNumbersApplication();
         String romanNumberResult = converter.convertToRomanFrom(numberTwo);
 
         assertThat(romanNumberResult).isSameAs("II");
+    }
+
+    @Test
+    public void should_number_three_converted_roman_numberic() throws Exception {
+        int numberThree = 3;
+
+        RomanNumbersApplication converter = new RomanNumbersApplication();
+        String romanNumberResult = converter.convertToRomanFrom(numberThree);
+
+        assertThat(romanNumberResult).isSameAs("III");
     }
 }
