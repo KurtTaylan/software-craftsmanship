ee3bf73fbf1e942221b6ee64dbc0090fcd3043e8
Taylan Kurt <taylankurt34@gmail.com>
2017-04-08 15:07:24 +0300

    9/9 are covered. Application and Test codes are refactored.

--- src/main/java/com/taylan/tdd/RomanNumbersApplication.java
+++ src/main/java/com/taylan/tdd/RomanNumbersApplication.java
@@ -8,25 +8,119 @@ package com.taylan.tdd;
 public class RomanNumbersApplication {
 
 
-    public String convertToRomanFrom(int number) {
+    public String runConverter(int inputValue) {
+        String output = "";
+        String numberInString = "" + inputValue;
 
+        for (int positionCounter = 1; positionCounter < numberInString.length() + 1; positionCounter++) {
+            int digit = Character.getNumericValue(numberInString.charAt(positionCounter - 1));
+            int value = numberInString.length() - ( positionCounter - 1 );
 
-        if (number == 1)
-            return  "I";
-        else if (number == 2)
-            return "II";
-        else if (number == 3)
-            return "III";
-        else if (number > 3 && number < 5)
-            return convertToRomanFrom(5-number) + "V"; // Left-side
-        else if (number == 5)
-            return "V";
-        else if (number > 5 && number < 10)
-            return "V"+ convertToRomanFrom(number - 5); // Right-side
-        else if (number == 10)
-            return "X";
-        else
-            return "I";
+            for (int i = 1; i < value; i ++)
+                digit *= 10;
 
+            String tempVa = convertToRomanFrom(digit);
+            output += tempVa;
+        }
+
+        return output;
     }
+
+
+    public String convertToRomanFrom(int inputNumber) {
+
+        if (inputNumber == 0) // Nothing to do about it
+            return "";
+
+        if (inputNumber > 3000) // No roman number can occur more than three times in row.
+            throw new IllegalArgumentException("This number is beyond than application scope ! Kırdın Beni gat damit 😤");
+
+
+        for (RomanConstants romanConstant : RomanConstants.values()) {
+
+            int value = romanConstant.value;
+            int backValue = romanConstant.backValue;
+            int backDifference = value - backValue;
+
+            int currentLevelDifference = inputNumber - value;
+
+            if (backDifference == inputNumber)
+                return getRomanNumber(backDifference); // Prefix;
+
+            if (currentLevelDifference >= 0) { // Suffix
+
+                return romanConstant.name() + convertToRomanFrom(currentLevelDifference);
+            }
+        }
+
+        throw new IllegalArgumentException("What did you do ?!! 🤕");
+    }
+
+
+    public String getRomanNumber(int number) {
+
+        switch (number) {
+            case 1000:
+                return RomanConstants.M.name();
+            case 900:
+                return getRomanNumber(100) + getRomanNumber(1000);
+            case 500:
+                return RomanConstants.D.name();
+            case 400:
+                return getRomanNumber(100) + getRomanNumber(500);
+            case 100:
+                return RomanConstants.C.name();
+            case 90:
+                return getRomanNumber(10) + getRomanNumber(100);
+            case 50:
+                return RomanConstants.L.name();
+            case 40:
+                return getRomanNumber(10) + getRomanNumber(50);
+            case 10:
+                return RomanConstants.X.name();
+            case 9:
+                return getRomanNumber(1) + getRomanNumber(10);
+            case 5:
+                return RomanConstants.V.name();
+            case 4:
+                return getRomanNumber(1) + getRomanNumber(5);
+            case 1:
+                return RomanConstants.I.name();
+        }
+
+        return "";
+    }
+
+
+    private enum RomanConstants {
+
+        M(1000, 100),
+        D(500, 100),
+        C(100, 10),
+        L(50, 10),
+        X(10, 1),
+        V(5, 1),
+        I(1, 1),
+        Error(0, 0);
+
+        private final int value;
+        private final int backValue;
+
+        RomanConstants(int value, int backValue) {
+            this.value = value;
+            this.backValue = backValue;
+        }
+
+
+        RomanConstants safeValueOf(final String candidate) {
+            try {
+
+                return RomanConstants.valueOf(candidate);
+
+            } catch (final IllegalArgumentException e) {
+                return Error;
+            }
+        }
+    }
+
 }
--- src/test/java/com/taylan/tdd/RomanNumbersApplicationTest.java
+++ src/test/java/com/taylan/tdd/RomanNumbersApplicationTest.java
@@ -1,84 +1,103 @@
 package com.taylan.tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Main Test
+ * Initial Test Class
  */
 public class RomanNumbersApplicationTest {
 
 
-    @Test
-    public void should_number_one_converted_roman_numeric_I() {
-        int numberOne = 1;
+    private RomanNumbersApplication converterApplication;
 
-        RomanNumbersApplication converter = new RomanNumbersApplication();
-        String romanNumberResult = converter.convertToRomanFrom(numberOne);
+
+    @Before
+    public void tearUp() {
+        converterApplication = new RomanNumbersApplication();
+    }
+
+
+    @Test
+    public void should_number_1_converted_roman_numeric_I() {
+        int inputNumber = 1;
+        String romanNumberResult = converterApplication.runConverter(inputNumber);
 
         assertThat(romanNumberResult).isEqualTo("I");
-
     }
 
     @Test
-    public void should_number_two_converted_roman_numeric_II() throws Exception {
-        int numberTwo = 2;
-
-        RomanNumbersApplication converter = new RomanNumbersApplication();
-        String romanNumberResult = converter.convertToRomanFrom(numberTwo);
+    public void should_number_2_converted_roman_numeric_II() throws Exception {
+        int inputNumber = 2;
+        String romanNumberResult = converterApplication.runConverter(inputNumber);
 
         assertThat(romanNumberResult).isEqualTo("II");
     }
 
     @Test
-    public void should_number_three_converted_roman_numeric_III() throws Exception {
-        int numberThree = 3;
-
-        RomanNumbersApplication converter = new RomanNumbersApplication();
-        String romanNumberResult = converter.convertToRomanFrom(numberThree);
+    public void should_number_3_converted_roman_numeric_III() throws Exception {
+        int inputNumber = 3;
+        String romanNumberResult = converterApplication.runConverter(inputNumber);
 
         assertThat(romanNumberResult).isEqualTo("III");
     }
 
     @Test
-    public void should_number_five_converted_roman_numeric_V() throws Exception {
-        int numberFive = 5;
-
-        RomanNumbersApplication converter = new RomanNumbersApplication();
-        String romanNumberResult = converter.convertToRomanFrom(numberFive);
+    public void should_number_5_converted_roman_numeric_V() throws Exception {
+        int inputNumber = 5;
+        String romanNumberResult = converterApplication.runConverter(inputNumber);
 
         assertThat(romanNumberResult).isEqualTo("V");
-
     }
 
     @Test
-    public void should_number_ten_converted_roman_numeric_X() throws Exception {
-        int numberTen = 10;
-
-        RomanNumbersApplication converter = new RomanNumbersApplication();
-        String romanNumberResult = converter.convertToRomanFrom(numberTen);
+    public void should_number_10_converted_roman_numeric_X() throws Exception {
+        int inputNumber = 10;
+        String romanNumberResult = converterApplication.runConverter(inputNumber);
 
         assertThat(romanNumberResult).isEqualTo("X");
     }
 
     @Test
-    public void should_number_six_converted_roman_numeric_VI() throws Exception {
-        int numberSix = 6;
-
-        RomanNumbersApplication converter = new RomanNumbersApplication();
-        String romanNumberResult = converter.convertToRomanFrom(numberSix);
+    public void should_number_6_converted_roman_numeric_VI() throws Exception {
+        int inputNumber = 6;
+        String romanNumberResult = converterApplication.runConverter(inputNumber);
 
         assertThat(romanNumberResult).isEqualTo("VI");
     }
 
     @Test
-    public void should_number_four_converted_roman_numeric_IV() throws Exception {
-        int numberSix = 4;
-
-        RomanNumbersApplication converter = new RomanNumbersApplication();
-        String romanNumberResult = converter.convertToRomanFrom(numberSix);
+    public void should_number_4_converted_roman_numeric_IV() throws Exception {
+        int inputNumber = 4;
+        String romanNumberResult = converterApplication.runConverter(inputNumber);
 
         assertThat(romanNumberResult).isEqualTo("IV");
+    }
+
+    @Test
+    public void should_number_50_converted_roman_numeric_L() throws Exception {
+        int inputNumber = 50;
+        String romanNumberResult = converterApplication.runConverter(inputNumber);
+
+        assertThat(romanNumberResult).isEqualTo("L");
+    }
+
+    @Test
+    public void should_number_43_converted_roman_numberic_XLIII() throws Exception {
+        int inputNumber = 43;
+        String romanNumberResult = converterApplication.runConverter(inputNumber);
+
+        assertThat(romanNumberResult).isEqualTo("XLIII");
+    }
+
+    @Test
+    public void should_number_1903_converted_roman_numberic_MCMIII() throws Exception {
+        int inputNumber = 1903;
+        String romanNumberResult = converterApplication.runConverter(inputNumber);
+
+        assertThat(romanNumberResult).isEqualTo("MCMIII");
+
     }
 }
