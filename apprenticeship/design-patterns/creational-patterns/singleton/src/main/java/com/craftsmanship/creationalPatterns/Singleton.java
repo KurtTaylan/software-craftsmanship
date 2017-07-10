d9e7d7699423b8101e3b4a1aa9d3c684dfc2b679
Taylan Kurt <taylan.kurt@iyzico.com>
2017-07-10 18:39:26 +0300

    Creational: Singleton Design pattern

---
+++ apprenticeship/design-patterns/creational-patterns/singleton/src/main/java/com/craftsmanship/creationalPatterns/Singleton.java
@@ -0,0 +1,18 @@
+package com.craftsmanship.creationalPatterns;
+
+public class Singleton {
+
+    private static volatile Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null)
+            synchronized (Singleton.class) {
+                if (instance == null)
+                    instance = new Singleton();
+            }
+        return instance;
+    }
+}
