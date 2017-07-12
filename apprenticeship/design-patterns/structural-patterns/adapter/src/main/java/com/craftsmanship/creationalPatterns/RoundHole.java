36007fd11c7d3eac4b61d03cb666c2b0262b534e
Taylan Kurt <taylan.kurt@iyzico.com>
2017-07-12 21:34:52 +0300

    Structural: Adapter design pattern

---
+++ apprenticeship/design-patterns/structural-patterns/adapter/src/main/java/com/craftsmanship/creationalPatterns/RoundHole.java
@@ -0,0 +1,16 @@
+package com.craftsmanship.creationalPatterns;
+
+/* The NEW */
+public class RoundHole {
+
+    private final int radius;
+
+    public RoundHole(int radius){
+        this.radius = radius;
+        System.out.println("RoundHole: max SquarePeg is " + radius * Math.sqrt(2));
+    }
+
+    public int getRadius(){
+        return radius;
+    }
+}
---
+++ apprenticeship/design-patterns/structural-patterns/adapter/src/main/java/com/craftsmanship/creationalPatterns/SquarePegAdapter.java
@@ -0,0 +1,22 @@
+package com.craftsmanship.creationalPatterns;
+
+// Design a "wrapper" class that can "impedance match" the old to the new
+public class SquarePegAdapter {
+
+    private final SquarePeg squarePeg;
+
+    public SquarePegAdapter(double w){
+        squarePeg = new SquarePeg(w);
+    }
+
+    //Identify the desired interface
+    public void makeFit(RoundHole roundHole){
+        // The adapter/wrapper class delegates to the legacy object
+        double amount = squarePeg.getWidth() - roundHole.getRadius() * Math.sqrt(2);
+        System.out.println( " reducing SquarePeg " + squarePeg.getWidth() + " by " + ( (amount < 0) ? 0 : amount ) + " amount");
+        if(amount > 0){
+            squarePeg.setWidth(squarePeg.getWidth() );
+            System.out.println("  width is now " + squarePeg.getWidth());
+        }
+    }
+}
