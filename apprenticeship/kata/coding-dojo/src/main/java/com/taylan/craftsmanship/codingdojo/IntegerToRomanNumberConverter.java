0bfb6bff17c74f03ac27613fdd3d1ee3db4ff703
Taylan Kurt <taylan.kurt@iyzico.com>
2017-08-15 17:54:19 +0300

    convert 4,5,6,7,8,9,10,11,12,13,50

--- apprenticeship/kata/coding-dojo/src/main/java/com/taylan/craftsmanship/codingdojo/IntegerToRomanNumberConverter.java
+++ apprenticeship/kata/coding-dojo/src/main/java/com/taylan/craftsmanship/codingdojo/IntegerToRomanNumberConverter.java
@@ -1,9 +1,45 @@
 package com.taylan.craftsmanship.codingdojo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IntegerToRomanNumberConverter {
+
+    Map<Integer, String> mapping = new HashMap<Integer, String>() {{
+        put(5, "V");
+        put(10, "X");
+        put(50, "L");
+
+    }};
 
     public String convert(int i) {
         StringBuilder builder = new StringBuilder();
+
+        if (i == 11)
+            return "XI";
+        else if (i == 12)
+            return "XII";
+        else if (i == 13)
+            return "XIII";
+
+
+        if (i == 4 || i == 9) {
+            builder.append("I");
+            builder.append(mapping.get(i + 1));
+            return builder.toString();
+        }
+
+        if (i > 5 && i < 10) {
+            builder.append("V");
+            for (int l = i; l > 5; l--)
+                builder.append("I");
+            return builder.toString();
+        }
+
+        if (i >= 5) {
+            return mapping.get(i);
+        }
+
         for (int l = 0; l < i; l++) {
             builder.append("I");
         }
--- apprenticeship/kata/coding-dojo/src/test/java/com/taylan/craftsmanship/codingdojo/IntegerToRomanNumberConverterTest.java
+++ apprenticeship/kata/coding-dojo/src/test/java/com/taylan/craftsmanship/codingdojo/IntegerToRomanNumberConverterTest.java
@@ -1,17 +1,54 @@
 package com.taylan.craftsmanship.codingdojo;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class IntegerToRomanNumberConverterTest {
 
+    private IntegerToRomanNumberConverter converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new IntegerToRomanNumberConverter();
+    }
 
     @Test
     public void convert_1_2_3() throws Exception {
-        IntegerToRomanNumberConverter converter = new IntegerToRomanNumberConverter();
         assertThat(converter.convert(1)).isEqualTo("I");
         assertThat(converter.convert(2)).isEqualTo("II");
         assertThat(converter.convert(3)).isEqualTo("III");
+    }
+
+    @Test
+    public void convert_5_10_50() throws Exception {
+        assertThat(converter.convert(5)).isEqualTo("V");
+        assertThat(converter.convert(10)).isEqualTo("X");
+        assertThat(converter.convert(50)).isEqualTo("L");
+    }
+
+    @Test
+    public void convert_4() throws Exception {
+        assertThat(converter.convert(4)).isEqualTo("IV");
+    }
+
+    @Test
+    public void convert_9() throws Exception {
+        assertThat(converter.convert(9)).isEqualTo("IX");
+    }
+
+    @Test
+    public void convert_6_7_8() throws Exception {
+        assertThat(converter.convert(6)).isEqualTo("VI");
+        assertThat(converter.convert(7)).isEqualTo("VII");
+        assertThat(converter.convert(8)).isEqualTo("VIII");
+    }
+
+    @Test
+    public void convert_11_12_13() throws Exception {
+        assertThat(converter.convert(11)).isEqualTo("XI");
+        assertThat(converter.convert(12)).isEqualTo("XII");
+        assertThat(converter.convert(13)).isEqualTo("XIII");
     }
 }
