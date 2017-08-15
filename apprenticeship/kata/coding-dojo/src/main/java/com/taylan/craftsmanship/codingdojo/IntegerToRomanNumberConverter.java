0e1637d1b6f94c80f86b9655225ec54279440669
Taylan Kurt <taylan.kurt@iyzico.com>
2017-08-15 18:29:09 +0300

    refactors conversion of 11,12,13

--- apprenticeship/kata/coding-dojo/src/main/java/com/taylan/craftsmanship/codingdojo/IntegerToRomanNumberConverter.java
+++ apprenticeship/kata/coding-dojo/src/main/java/com/taylan/craftsmanship/codingdojo/IntegerToRomanNumberConverter.java
@@ -6,42 +6,48 @@ import java.util.Map;
 public class IntegerToRomanNumberConverter {
 
     Map<Integer, String> mapping = new HashMap<Integer, String>() {{
+        put(1, "I");
         put(5, "V");
         put(10, "X");
         put(50, "L");
 
     }};
 
-    public String convert(int i) {
+    public String convert(int input) {
         StringBuilder builder = new StringBuilder();
 
-        if (i == 11)
-            return "XI";
-        else if (i == 12)
-            return "XII";
-        else if (i == 13)
-            return "XIII";
+        if (mapping.containsKey(input)) {
+            return mapping.get(input);
+        }
 
-        if (i == 4 || i == 9) {
+        if (input == 4 || input == 9) {
             builder.append("I");
-            builder.append(mapping.get(i + 1));
+            builder.append(mapping.get(input + 1));
             return builder.toString();
         }
 
-        if (i > 5 && i < 10) {
-            builder.append("V");
-            for (int l = i; l > 5; l--)
+        if (input > 10) {
+            builder.append("X");
+            for (int i = 0; i < input - 10; i++) {
                 builder.append("I");
+            }
             return builder.toString();
         }
 
-        if (i >= 5) {
-            return mapping.get(i);
+        if (input > 5) {
+            builder.append("V");
+            for (int i = 0; i < input - 5; i++) {
+                builder.append("I");
+            }
+            return builder.toString();
         }
 
-        for (int l = 0; l < i; l++) {
+        if (input > 1) {
             builder.append("I");
+            for (int l = 0; l < input - 1; l++) {
+                builder.append("I");
+            }
         }
-        return builder.toString();
+        9return builder.toString();
     }
 }
