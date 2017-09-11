acb041a3f83637f28ef6c10eb19881be8bc321fd
Taylan Kurt <taylan.kurt@iyzico.com>
2017-09-11 15:27:16 +0300

    Behavioral: Memento design pattern

---
+++ apprenticeship/design-patterns/behavioural-pattern/memento/src/main/java/com/craftsmanship/behavioralPatterns/example1/Memento.java
@@ -0,0 +1,13 @@
+package com.craftsmanship.behavioralPatterns.example1;
+
+class Memento {
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
