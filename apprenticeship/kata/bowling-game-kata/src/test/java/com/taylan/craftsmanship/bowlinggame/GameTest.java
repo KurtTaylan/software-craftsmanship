5f62871ae5909bf8aec45f60bab75d8f58c9adb6
İbrahim Kahraman <ibrahim.kahraman@iyzico.com>
2017-09-18 19:20:57 +0300

    adds should_roll_twice

--- apprenticeship/kata/bowling-game-kata/src/test/java/com/taylan/craftsmanship/bowlinggame/GameTest.java
+++ apprenticeship/kata/bowling-game-kata/src/test/java/com/taylan/craftsmanship/bowlinggame/GameTest.java
@@ -7,12 +7,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GameTest {
 
     @Test
-    public void should_roll_number_one() {
+    public void should_roll_once() {
         Game game = new Game();
         Frame frame = new Frame();
         frame.doRolling(3);
         game.addFrame(frame);
 
         assertEquals(3, game.getScore());
+    }
+
+    @Test
+    public void should_roll_twice() {
+        Game game = new Game();
+        Frame frame = new Frame();
+        frame.doRolling(3);
+        frame.doRolling(5);
+        game.addFrame(frame);
+
+        assertEquals(8, game.getScore());
     }
 }
